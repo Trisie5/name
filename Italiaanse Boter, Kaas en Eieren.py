f7372684b59d9ce82dd1fbfa7e26100b288fd603
Trisie5 <tristan.burght@gmail.com>
2023-10-16 17:21:28 +0200

    drieopeenrei function

--- Italiaanse Boter, Kaas en Eieren.py
+++ Italiaanse Boter, Kaas en Eieren.py
@@ -4,6 +4,18 @@ def print_bord():
     print(plaats[6], plaats[7], plaats[8])
 
 
+def drieopeenrei(speler):
+    if (plaats[0] == speler and plaats[1] == speler and plaats[2] == speler) or (
+            plaats[3] == speler and plaats[4] == speler and plaats[5] == speler) or (
+            plaats[6] == speler and plaats[7] == speler and plaats[8] == speler) or (
+            plaats[0] == speler and plaats[3] == speler and plaats[6] == speler) or (
+            plaats[1] == speler and plaats[4] == speler and plaats[7] == speler) or (
+            plaats[2] == speler and plaats[5] == speler and plaats[8] == speler) or (
+            plaats[0] == speler and plaats[4] == speler and plaats[8] == speler) or (
+            plaats[2] == speler and plaats[4] == speler and plaats[6] == speler):
+        return True
+
+
 spelactief = True
 while spelactief:
     print("Welkom bij Italiaanse Boter, Kaas en Eieren")
@@ -25,108 +37,77 @@ while spelactief:
                 print("Deze plaats is bezet")
             print_bord()
         while True:
-            if (plaats[0] == "X" and plaats[1] == "X" and plaats[2] == "X") or (
-                    plaats[3] == "X" and plaats[4] == "X" and plaats[5] == "X") or (
-                    plaats[6] == "X" and plaats[7] == "X" and plaats[8] == "X") or (
-                    plaats[0] == "X" and plaats[3] == "X" and plaats[6] == "X") or (
-                    plaats[1] == "X" and plaats[4] == "X" and plaats[7] == "X") or (
-                    plaats[2] == "X" and plaats[5] == "X" and plaats[8] == "X") or (
-                    plaats[0] == "X" and plaats[4] == "X" and plaats[8] == "X") or (
-                    plaats[2] == "X" and plaats[4] == "X" and plaats[6] == "X"):
+            if drieopeenrei("X"):
+                break
+            plaatsSpeler = input(f"Waar wil {naamSpeler2} een steen plaatsen?")
+            if plaatsSpeler not in plaatsen:
+                print("Antwoord met een plaats tussen de 1 en 9")
+            elif plaatsSpeler in plaats:
+                plaats[int(plaatsSpeler) - 1] = "O"
+                print_bord()
                 break
             else:
-                plaatsSpeler = input(f"Waar wil {naamSpeler2} een steen plaatsen?")
-                if plaatsSpeler not in plaatsen:
-                    print("Antwoord met een plaats tussen de 1 en 9")
-                elif plaatsSpeler in plaats:
-                    plaats[int(plaatsSpeler) - 1] = "O"
-                    print_bord()
-                    break
-                else:
-                    print("Deze plaats is bezet")
-                print_bord()
+                print("Deze plaats is bezet")
+            print_bord()
     geenDrieOpEenRij = True
     while geenDrieOpEenRij:
-        if (plaats[0] == "X" and plaats[1] == "X" and plaats[2] == "X") or (
-                plaats[3] == "X" and plaats[4] == "X" and plaats[5] == "X") or (
-                plaats[6] == "X" and plaats[7] == "X" and plaats[8] == "X") or (
-                plaats[0] == "X" and plaats[3] == "X" and plaats[6] == "X") or (
-                plaats[1] == "X" and plaats[4] == "X" and plaats[7] == "X") or (
-                plaats[2] == "X" and plaats[5] == "X" and plaats[8] == "X") or (
-                plaats[0] == "X" and plaats[4] == "X" and plaats[8] == "X") or (
-                plaats[2] == "X" and plaats[4] == "X" and plaats[6] == "X"):
+        if drieopeenrei("X"):
             print(f"{naamSpeler1} heeft gewonnen")
             break
-        if (plaats[0] == "O" and plaats[1] == "O" and plaats[2] == "O") or (
-                plaats[3] == "O" and plaats[4] == "O" and plaats[5] == "O") or (
-                plaats[6] == "O" and plaats[7] == "O" and plaats[8] == "O") or (
-                plaats[0] == "O" and plaats[3] == "O" and plaats[6] == "O") or (
-                plaats[1] == "O" and plaats[4] == "O" and plaats[7] == "O") or (
-                plaats[2] == "O" and plaats[5] == "O" and plaats[8] == "O") or (
-                plaats[0] == "O" and plaats[4] == "O" and plaats[8] == "O") or (
-                plaats[2] == "O" and plaats[4] == "O" and plaats[6] == "O"):
+        if drieopeenrei("O"):
             print(f"{naamSpeler2} heeft gewonnen")
             break
-        else:
-            verplaatsjuist = False
-            while not verplaatsjuist:
-                verplaats = input(f"Welke steen wil {naamSpeler1} verplaatsen")
-                if verplaats not in plaatsen:
-                    print("Antwoord met een plaats tussen de 1 en 9")
-                    print_bord()
-                elif plaats[int(verplaats) - 1] != "X":
-                    print("Hier staat geen steen van jou")
-                    print_bord()
-                else:
-                    print_bord()
-                    while True:
-                        plaatsSpeler = input("Waar wil je deze steen plaatsen?")
-                        if plaatsSpeler not in plaatsen:
-                            print("Antwoord met een plaats tussen de 1 en 9")
-                        elif plaatsSpeler in plaats:
-                            plaats[int(verplaats) - 1] = verplaats
-                            plaats[int(plaatsSpeler) - 1] = "X"
-                            verplaatsjuist = True
-                            print_bord()
-                            break
-                        else:
-                            print("Deze plaats is bezet")
-                        print_bord()
-            if (plaats[0] == "X" and plaats[1] == "X" and plaats[2] == "X") or (
-                    plaats[3] == "X" and plaats[4] == "X" and plaats[5] == "X") or (
-                    plaats[6] == "X" and plaats[7] == "X" and plaats[8] == "X") or (
-                    plaats[0] == "X" and plaats[3] == "X" and plaats[6] == "X") or (
-                    plaats[1] == "X" and plaats[4] == "X" and plaats[7] == "X") or (
-                    plaats[2] == "X" and plaats[5] == "X" and plaats[8] == "X") or (
-                    plaats[0] == "X" and plaats[4] == "X" and plaats[8] == "X") or (
-                    plaats[2] == "X" and plaats[4] == "X" and plaats[6] == "X"):
-                print(f"{naamSpeler1} heeft gewonnen")
-                break
+        verplaatsjuist = False
+        while not verplaatsjuist:
+            verplaats = input(f"Welke steen wil {naamSpeler1} verplaatsen")
+            if verplaats not in plaatsen:
+                print("Antwoord met een plaats tussen de 1 en 9")
+                print_bord()
+            elif plaats[int(verplaats) - 1] != "X":
+                print("Hier staat geen steen van jou")
+                print_bord()
             else:
-                verplaatsjuist = False
-                while not verplaatsjuist:
-                    verplaats = input(f"Welke steen wil {naamSpeler2} verplaatsen")
-                    if verplaats not in plaatsen:
+                print_bord()
+                while True:
+                    plaatsSpeler = input("Waar wil je deze steen plaatsen?")
+                    if plaatsSpeler not in plaatsen:
                         print("Antwoord met een plaats tussen de 1 en 9")
+                    elif plaatsSpeler in plaats:
+                        plaats[int(verplaats) - 1] = verplaats
+                        plaats[int(plaatsSpeler) - 1] = "X"
+                        verplaatsjuist = True
                         print_bord()
-                    elif plaats[int(verplaats) - 1] != "O":
-                        print("Hier staat geen steen van jou")
-                        print_bord()
+                        break
                     else:
+                        print("Deze plaats is bezet")
+                    print_bord()
+        if drieopeenrei("X"):
+            print(f"{naamSpeler1} heeft gewonnen")
+            break
+        verplaatsjuist = False
+        while not verplaatsjuist:
+            verplaats = input(f"Welke steen wil {naamSpeler2} verplaatsen")
+            if verplaats not in plaatsen:
+                print("Antwoord met een plaats tussen de 1 en 9")
+                print_bord()
+            elif plaats[int(verplaats) - 1] != "O":
+                print("Hier staat geen steen van jou")
+                print_bord()
+            else:
+                print_bord()
+                while True:
+                    plaatsSpeler = input("Waar wil je deze steen plaatsen?")
+                    if plaatsSpeler not in plaatsen:
+                        print("Antwoord met een plaats tussen de 1 en 9")
+                    elif plaatsSpeler in plaats:
+                        plaats[int(verplaats) - 1] = verplaats
+                        plaats[int(plaatsSpeler) - 1] = "O"
+                        verplaatsjuist = True
                         print_bord()
-                        while True:
-                            plaatsSpeler = input("Waar wil je deze steen plaatsen?")
-                            if plaatsSpeler not in plaatsen:
-                                print("Antwoord met een plaats tussen de 1 en 9")
-                            elif plaatsSpeler in plaats:
-                                plaats[int(verplaats) - 1] = verplaats
-                                plaats[int(plaatsSpeler) - 1] = "O"
-                                verplaatsjuist = True
-                                print_bord()
-                                break
-                            else:
-                                print("Deze plaats is bezet")
-                            print_bord()
+                        break
+                    else:
+                        print("Deze plaats is bezet")
+                    print_bord()
     antwoordjuist = False
     while not antwoordjuist:
         antwoord = input("Wil je nog een keer spelen?")
